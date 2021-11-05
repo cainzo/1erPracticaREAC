ec1e09aebcf431e33d07016f48ad1ff07fec3888
GonzaloC <cainzog@gmail.com>
2021-11-05 08:03:04 -0300

    se agrego el maquetado del item Lista tarea y los componens listaTarea, ItemTarea

---
+++ src/components/ListaTareas.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ItemTarea from "./ItemTarea";
+
+const ListaTareas = () => {
+  return (
+    <div>
+      <ul class="list-group my-5">
+        <ItemTarea></ItemTarea>
+      </ul>
+    </div>
+  );
+};
+
+export default ListaTareas;
