afb0fb92403bd5f5acc7e4be10c0bd5ba74d4f2f
GonzaloC <cainzog@gmail.com>
2021-11-05 07:38:05 -0300

    agregamos estetica  alos componentes titulo y subtirulos y ademas agregamos dos props

---
+++ src/components/Subtitulo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Subtitulo = (props) => {
+    // aqui va la logica 
+
+    return (
+        // aqui va el maquetado y un poco de logica
+        <h2 className='text-center text-light'>Tareas de la comision {props.comision}</h2>
+    )
+}
+
+export default Subtitulo;
